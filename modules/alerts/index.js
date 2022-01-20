1bc7ad40b8042bcc06fb967a31a971e30dddb16f
Lucas M. D <lucas@absolab.xyz>
2022-01-20 19:45:22 -0300

    fix: corrige eventos emitidos

--- modules/alerts/index.js
+++ modules/alerts/index.js
@@ -17,12 +17,12 @@ const Alerts = ({ client, app, io }) => {
 
   client.on('cheer', (channel, user) => {
     client.say(channel, `Obrigado pelos ${user.bits} bits ${user['display-name']}!!!`);
-    io.emit('resub', { username: user['display-name'], bits: user.bits });
+    io.emit('cheer', { username: user['display-name'], bits: user.bits });
   });
 
   client.on('raided', (channel, username) => {
     client.say(channel, `Obrigado pela raid ${username}, sejam todos bem vindos!`);
-    io.emit('resub', { username });
+    io.emit('raided', { username });
   });
 };
 
